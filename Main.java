7bde8ad55e6a3fe5e5337ab23befa65e32d0767b
NateOnderko <145400200+NateOnderko@users.noreply.github.com>
2023-09-18 19:49:50 -0400

    Main.java
    1.3 Update

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
-public class Main {
+import java.util.Scanner;
 
+
+public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.print("Goodbye World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter two integers.");
+
+        final int num1 = s.nextInt(); // Only num1 is final; num2 can still be changed.
+        int num2 = s.nextInt();
+
+        s.close();
+
+        num2++; // Adds 1 to num2
+
+        try {
+            System.out.print(num1 / num2);
+        }
+        catch (ArithmeticException e) {
+            System.out.print("Can't divide by zero.");
+        }
 
     }
 }
